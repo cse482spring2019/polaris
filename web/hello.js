da2c7c6d525cc0ed613786a4d83c75939d5242cd
Kyle <pierceka@cs.washington.edu>
2019-05-16 12:39:50 -0700

    better tags

--- web/hello.js
+++ web/hello.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
-	var n = ["Tight Spaces",
-		"Sharp Inclines",
-		"Construction Nearby",
-		"Low-Quality Sidewalk",
-		"Overgrown Foilage",
-		"Lack of Elevator",
-		"Broken Benches",
-		"Insufficient Light"];
-	var c = [11, 42, 91, 1, 0, 77, 14, 0];
-	var c2 = new Array(c.length).fill(0);
+	var data = [{name: "Tight Spaces", count: 11},
+		        {name: "Sharp Inclines", count: 42},
+		        {name: "Construction Nearby", count: 91},
+		        {name: "Low-Quality Sidewalk", count: 1},
+		        {name: "Overgrown Foilage", count: 0},
+		        {name: "Lack of Elevator", count: 77},
+		        {name: "Broken Benches", count: 14},
+		        {name: "Insufficient Light", count: 0}];
+	var added = new Array(data.length).fill(0);
+
+    data.sort(function(a, b) { return b.count - a.count; });
+
 	var images = ['a.jpg', 'b.jpg']
 	var editing = false;
 
 	/* Puts the initial buttons on the page */
-	for (var i = 0; i < n.length; i++) {
+	for (var i = 0; i < data.length; i++) {
 		$('.tags').append(
-			'<button type="button" id="btn-' + i + '" class="tag" disabled></button>'
+			'<button type="button" id="btn-' + i + '" class="' + getClass(i) + '" disabled></button>'
 		);
-
-		var btn = $('#btn-' + i);
-		btn.html(format(n[i], c[i]));
-		if (c[i] == 0) {
-			btn.hide();
-		}
+		$('#btn-' + i).html(format(data[i].name, data[i].count));
 	}
 
 	for (var i = 0; i < images.length; i++) {
@@ -42,15 +39,15 @@ $(document).ready(function() {
 	/* Behavior for clicking on a tag */
 	$(".tag").click(function() {
 		if (editing) {
-			var i     = $(this).attr('id').charAt(4);
-			if ($(this).css('background-color') == 'rgb(116, 187, 56)') {
-				$(this).css('background-color', '#487623');
-				$(this).html(format(n[i], c[i] + 1));
-				c2[i] = 1;
+			var i = $(this).attr('id').charAt(4);
+			if (!$(this).attr('class').includes('tag-selected')) {
+				added[i] = 1;
+                $(this).attr('class', 'tag tag-selected');
+				$(this).html(format(data[i].name, data[i].count + 1));
 			} else {
-				$(this).css('background-color', '#74BB38');
-				$(this).html(format(n[i], c[i]));
-				c2[i] = 0;
+				added[i] = 0;
+                $(this).attr('class', getClass(i));
+				$(this).html(format(data[i].name, data[i].count));
 			}
 		}
 	});
@@ -61,12 +58,12 @@ $(document).ready(function() {
 		$('#edit').attr('disabled', true);
 		editing = true;
 
-		for (var i = 0; i < n.length; i++) {
+		for (var i = 0; i < data.length; i++) {
 			var btn = $('#btn-' + i);
 			btn.attr('disabled', false);
 			btn.show();
-			if (c2[i] == 1) {
-				btn.css('background-color', '#487623');
+			if (added[i] == 1) {
+                btn.attr('class', 'tag tag-selected');
 			}
 		}
 	});
@@ -77,14 +74,11 @@ $(document).ready(function() {
 		$('#edit').attr('disabled', false);
 		editing = false;
 
-		for (var i = 0; i < n.length; i++) {
+		for (var i = 0; i < data.length; i++) {
 			var btn = $('#btn-' + i);
 			btn.attr('disabled', true);
-			if (c[i] + c2[i] == 0) {
-				btn.hide();
-			}
-			btn.html(format(n[i], c[i] + c2[i]))
-			btn.css('background-color', '#74BB38');
+			btn.html(format(data[i].name, data[i].count + added[i]))
+            btn.attr('class', getClass(i));
 		}
 
 		//TODO; send the data to the service
@@ -113,4 +107,8 @@ $(document).ready(function() {
 	function format(name, count) {
 		return name + ' (' + count + ')';
 	}
+
+    function getClass(i) {
+        return (data[i].count + added[i]) == 0 ? 'tag tag-zero' : 'tag tag-default';
+    }
 });

dff716024bc5c18fb0139f1ffed6f32d185d27b1
Tyler Mi <gartymi@cs.washington.edu>
2019-06-02 22:19:54 -0700

    Code for first use

--- Application.java
+++ Application.java
@@ -10,15 +10,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.util.Assert;
-
+// The first time you are running the database, replace Application.java with this and run it once.
 @SpringBootApplication
-public class PolarisDatabaseApplication implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 
     @Autowired
     private StopRepository repository;
 
     public static void main(String[] args) {
-        SpringApplication.run(PolarisDatabaseApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     public void run(String... var1) throws Exception {
